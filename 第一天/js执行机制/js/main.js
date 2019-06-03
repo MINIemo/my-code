d787860f5440d2e4f952997610ff2b36a943b12f
阿毛蛋蛋 <1226927180@qq.com>
2019-06-03 17:06:57 +0800

    a

---
+++ 第一天/js执行机制/js/main.js
@@ -0,0 +1,25 @@
+
+
+console.log(1);
+
+setTimeout(() => {
+    console.log(5);
+}, 0);
+
+new Promise(function (resolve, reject) {
+    console.log(2);
+
+    for (let i = 0; i < 999999999; i++) {
+        if (i === 10000000) {
+            console.log(7);
+        }
+    }
+
+    resolve();
+
+    console.log(4);
+}).then(() => {
+    console.log(3);
+})
+
+console.log(6);
