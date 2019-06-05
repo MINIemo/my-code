5caf1c249d38f645733a91ca6f05c33db0cb2167
阿毛蛋蛋 <1226927180@qq.com>
2019-06-05 11:14:59 +0800

    a

--- 第三天/事件处理与修饰符/js/main.js
+++ 第三天/事件处理与修饰符/js/main.js
@@ -1,9 +1,22 @@
 
 // 事件处理与修饰符
 
+// 修饰符：指令的修饰符，. 后面添加修饰符，作用是给 指令添加额外功能
+
+// 事件处理的修饰符：.prevent，.stop，.once
 
 const vm = new Vue({
     el: '#app',
     data: {
+        count: 0
+    },
+    methods: {
+        increase(val, e) {
+            // 默认会把事件对象传过来
+            console.log(val, e);
+
+            this.count++;
+
+        }
     }
 });
