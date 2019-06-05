c072921a8f5c9d8b6cc58d2371c3ef2c09b745c8
阿毛蛋蛋 <1226927180@qq.com>
2019-06-05 14:49:37 +0800

    a

--- 第三天/表单输入绑定/js/main.js
+++ 第三天/表单输入绑定/js/main.js
@@ -1,9 +1,18 @@
 
-// 表单输入绑定
+// 表单输入绑定（v-model）
+// v-model：用于将 数据与表单元素的 value属性 创建 双向绑定，指令值 必须是 某个 data属性
 
 const vm = new Vue({
     el: '#app',
     data: {
-
+        username: '请输入用户名',
+        text: '我是一段文本内容',
+        password: '',
+        radio: '1',
+        check: ['1', '4'],
+        check1: true,
+        select: '3',
+        select1: ['1', '4'],
+        select2: '5'
     }
 });
