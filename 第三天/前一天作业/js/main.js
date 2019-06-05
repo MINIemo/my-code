c98f9c3d6ec71094de12e842de4d0aa1f3013df7
阿毛蛋蛋 <1226927180@qq.com>
2019-06-05 13:48:58 +0800

    a

--- 第三天/前一天作业/js/main.js
+++ 第三天/前一天作业/js/main.js
@@ -2,6 +2,31 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        
+        list: [
+            {
+                title: '我是标题1',
+                isDone: true
+            },
+            {
+                title: '我是标题2',
+                isDone: false
+            },
+            {
+                title: '我是标题3',
+                isDone: false
+            },
+            {
+                title: '我是标题4',
+                isDone: true
+            }
+        ]
+    },
+    methods: {
+        del(index) {
+            this.list.splice(index, 1);
+        },
+        setStatus(index) {
+            this.list[index].isDone = !this.list[index].isDone;
+        }
     }
 });
