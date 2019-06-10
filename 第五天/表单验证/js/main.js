08a24012a83b0becc93e68b61bdec4cd6a5d5228
阿毛蛋蛋 <1226927180@qq.com>
2019-06-10 10:04:05 +0800

    a

--- 第五天/表单验证/js/main.js
+++ 第五天/表单验证/js/main.js
@@ -1,7 +1,60 @@
 
 
+const reg = {
+    username: /^[a-zA-Z0-9_]{4,16}$/,
+    email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+};
+
 const vm = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        user: {
+            username: '',
+            email: ''
+        },
+        // verification: {
+        //     username: false,
+        //     email: false
+        // },
+        list: [
+
+        ]
+    },
+    // watch: {
+    //     'user.username'() {
+    //         this.verification.username = !reg.username.test(this.user.username);
+    //     },
+    //     'user.email'() {
+    //         this.verification.email = !reg.email.test(this.user.email);
+    //     }
+    // },
+
+    computed: {
+        verification() {
+            const obj = {};
+            if (this.user.username) {
+                obj.username = !reg.username.test(this.user.username);
+            }
+            if (this.user.email) {
+                obj.email = !reg.email.test(this.user.email);
+            }
+            return obj;
+        }
+    },
+    methods: {
+        add() {
+            if (this.user.username && this.user.email) {
+                if (!this.verification.username && !this.verification.email) {
+                    this.list.push({
+                        username: this.user.username,
+                        email: this.user.email
+                    });
+                    this.user.username = '';
+                    this.user.email = '';
+                }
+            }
+        }
+    }
 });
 
 
@@ -18,14 +71,3 @@ const vm = new Vue({
 
 
 
-
-
-
-
-
-
-
-// const reg = {
-//     username: /^[a-zA-Z0-9_]{4,16}$/,
-//     email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-// };
